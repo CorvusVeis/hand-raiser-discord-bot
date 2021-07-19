31363daae05e44812ccdce462d5258465a2222fd
Shawn Kays <shawngk@gmail.com>
2021-07-18 23:58:38 -0500

    Ready message is sent to specified channel.

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,3 +1,4 @@
-module.exports = () => {
-    console.log('hand-raiser bot is online.');
+module.exports = (Discord, client) => {
+    console.log('hand-raiser-bot is online.');
+    client.channels.cache.get(process.env.CHANNEL_ID_FOR_READY_MESSAGE).send("hand-raiser-bot is online.");
 }
